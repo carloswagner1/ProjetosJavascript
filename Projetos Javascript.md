ce48653f0ab6807adfe084bb3a9d876e551f9436
Carlos Wagner <61124810+carloswagner1@users.noreply.github.com>
2022-01-11 08:54:56 -0300

    furst commit

---
+++ Projetos Javascript.md
@@ -0,0 +1,16 @@
+# Projetos Javascript	
+
+#### Este repositório tem o objetivo de compilar projetos escritos em Javascript, associados com HTML/CSS realizados como exercícios para desenvolvimento de habilidades, servindo como portfólio de desenvolvimento. Abaixo será listado o nome do projeto e, quando possível, um link para testar a aplicação:
+
+1. Urna Eletrônica (SImulação) - [voto.netlify.app](https://voto.netlify.app/)
+
+
+
+
+
+
+
+
+
+
+
---
+++ Urna/etapas.js
@@ -0,0 +1,52 @@
+let etapas =[
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'18001',
+                nome: 'Marina Silva',
+                partido: 'Rede Sustentabilidade',
+                fotos:[
+                    {url:'Marina.png', legenda: 'Deputado Federal', small:false}
+                ]
+            },
+            {
+                numero:'51001',
+                nome: 'Cabo Daciolo',
+                partido: 'Patriotas',
+                fotos:[
+                    {url:'daciolo.png', legenda: 'Deputado Federal', small:false}
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:[
+        {
+            numero: '13',
+            nome: 'Lula',
+            partido: 'PT',
+            vice:'Geraldo Alkmin',
+            fotos:[
+                {url: 'lula.png', legenda: 'Presidente'},
+                {url: 'Geraldo.png', legenda: 'Vice', small: true}
+            ]
+
+        },
+        {
+            numero: '22',
+            nome: 'Jair Bolsonaro',
+            partido: 'PL',
+            vice:'Amoedo',
+            fotos:[
+                {url: 'JB.png', legenda: 'Presidente'},
+                {url: 'Amoedo.png', legenda: 'Vice', small: true}
+            ]
+        },
+        ]
+    }
+];
